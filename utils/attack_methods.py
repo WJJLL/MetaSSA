3e80b011ce0f2e1a78bf6648106f96a4d734b5f5
Juanjuan Weng <47670225+WJJLL@users.noreply.github.com>
2023-11-23 12:33:41 +0800

    Update attack_methods.py

--- utils/attack_methods.py
+++ utils/attack_methods.py
@@ -35,28 +35,8 @@ def DI(x, resize_rate=1.15, diversity_prob=0.5):
     pad_right = w_rem - pad_left
     padded = F.pad(rescaled, [pad_left.item(), pad_right.item(), pad_top.item(), pad_bottom.item()], value=0)
     ret = padded if torch.rand(1) < diversity_prob else x
+    ret = transforms.Resize((img_size, img_size), interpolation=InterpolationMode.NEAREST)(ret)
     return ret
-
-def RDI(x, resize_rate=1.15, diversity_prob=0.5):
-    assert resize_rate >= 1.0
-    assert diversity_prob >= 0.0 and diversity_prob <= 1.0
-    img_size = x.shape[-1]
-    img_resize = int(img_size * resize_rate)
-    rnd = torch.randint(low=img_size, high=img_resize, size=(1,), dtype=torch.int32)
-    rescaled = F.interpolate(x, size=[rnd, rnd], mode='bilinear', align_corners=False)
-    h_rem = img_resize - rnd
-    w_rem = img_resize - rnd
-    pad_top = torch.randint(low=0, high=h_rem.item(), size=(1,), dtype=torch.int32)
-    pad_bottom = h_rem - pad_top
-    pad_left = torch.randint(low=0, high=w_rem.item(), size=(1,), dtype=torch.int32)
-    pad_right = w_rem - pad_left
-    padded = F.pad(rescaled, [pad_left.item(), pad_right.item(), pad_top.item(), pad_bottom.item()], value=0)
-    ret = padded if torch.rand(1) < diversity_prob else x
-
-    if img_size>64: # For the CIFAR-10 dataset, we skip the image size reduction.
-        ret = transforms.Resize((img_size, img_size), interpolation=InterpolationMode.NEAREST)(ret)
-    return ret
-
 
 
 class CropShift(torch.nn.Module):
